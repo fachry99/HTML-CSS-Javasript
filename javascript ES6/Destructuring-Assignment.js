e702c43dc64306c27b6182446e3029ab8b460d02
fachry99 <151fachry@gmail.com>
2023-08-23 10:43:08 +0700

    membuat beberapa contoh kondisi pada destructuring

--- javascript ES6/Destructuring-Assignment.js
+++ javascript ES6/Destructuring-Assignment.js
@@ -1,8 +1,43 @@
-//Destructuring variable / assignment
+//*Destructuring variable / assignment
 
 //Destructuring Array
 
 const perkenalan = ['Halo', 'nama', 'saya', 'Rizky'];
 
-const [salam, satu, dua, nama] = perkenalan;
-console.log(salam);
+//const [salam, satu, dua, nama] = perkenalan;
+//skip items
+// const [salam, , , nama] = perkenalan;
+// console.log(nama);
+
+//*swap items / tukar nilai
+// let a = 1;
+// let b = 2;
+// console.log(a);
+// [a, b] = [b, a];
+// console.log(a);
+
+//*return value pada function
+// function coba() {
+//     return [1, 2];
+// }
+// const [a, b] = coba();
+// console.log(a);
+
+//*pake rest parameter kalo array nya banyak
+// const [a, ...values] = [1, 2, 3, 4, 5];
+// console.log(a);
+// console.log(values);
+
+//*Destructuring Object
+// const mhs = {
+//     nama: 'Rizky',
+//     umur: 20,
+//     tinggal: "Jakarta"
+// }
+
+// const { nama, umur, tinggal } = mhs;
+// console.log(tinggal);
+
+//*Assignment tanpa deklarasi object = harus ada dalam kurung
+({ nama, umur, tinggal } = {nama: 'Rizky', umur: 20, tinggal: "Jakarta"});
+console.log(nama);
