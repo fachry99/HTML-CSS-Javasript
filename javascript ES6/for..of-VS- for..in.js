3c62bb8a1909ee9802f85c46a3f79aa0cdc7a0bf
fachry99 <151fachry@gmail.com>
2023-08-23 16:36:53 +0700

    update 2 file

--- javascript ES6/for..of-VS- for..in.js
+++ javascript ES6/for..of-VS- for..in.js
@@ -1,6 +1,32 @@
 //* for..of
 
+// const mhs = ['Sandhika', 'Doddy', 'Erik'];
+
+// for(let i= 0; i < mhs.length; i++) {
+//     console.log(mhs[i]);
+// }
+
+//? versi forEnch
+//mhs.forEach(m => console.log(m));
+
+
+//? versi for..of
+// for(const m of mhs) {
+//     console.log(m);
+// }       
+
+//? versi string
+// const nama =  'Sandhika';
+// for (const s of nama) {
+//     console.log(s);
+// }
+
 const mhs = ['Sandhika', 'Doddy', 'Erik'];
-for(let i= 0; i < mhs.length; i++) {
-    console.log(mhs[i]);
+//mhs.forEach((m, i) => console.log(`${m} adalah mahasiswa ke-${i + 1}`));
+
+//for of tidak bisa mengambil index, jadi harus menggunakan method entries agar bisa mengambil index
+for (const[i, m]of mhs.entries()) {
+    console.log(`${m} adalah mahasiswa ke-${i + 1}`);
 }
+
+//* Node list
