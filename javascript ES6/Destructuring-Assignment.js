f88b8a3a65a952feb49336086d0849d2df9cbbdc
fachry99 <151fachry@gmail.com>
2023-08-23 10:07:00 +0700

    menambahkan file destructuring.js

---
+++ javascript ES6/Destructuring-Assignment.js
@@ -0,0 +1,8 @@
+//Destructuring variable / assignment
+
+//Destructuring Array
+
+const perkenalan = ['Halo', 'nama', 'saya', 'Rizky'];
+
+const [salam, satu, dua, nama] = perkenalan;
+console.log(salam);
