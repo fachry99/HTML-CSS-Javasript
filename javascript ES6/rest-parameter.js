d91d43d6f0f56d454d1f70c7c56773d069f81664
fachry99 <151fachry@gmail.com>
2023-08-26 09:09:45 +0700

    update contoh kasus

--- javascript ES6/rest-parameter.js
+++ javascript ES6/rest-parameter.js
@@ -1,11 +1,34 @@
 //* rest prameter
-function jumlahkan(...angka){
-    let total = 0;
-    for(const a of angka){
-    total += a;
-    }
-    return total;
-    return angka.reduce((a, b) => a + b);
-}
+//function jumlahkan(...angka){
+    // let total = 0;
+    // for(const a of angka){
+    // total += a;
+    // }
+    // return total; //?versi biasa
+    //return angka.reduce((a, b) => a + b); //?versi hight order function
+//}
 
-console.log(jumlahkan(1, 2, 3, 4, 5));
+//console.log(jumlahkan(1, 2, 3, 4, 5));
+
+//* array destructuring
+// const kelompok1 = ['Doda', 'Dofa', 'Dumang', 'asep', 'Dodi'];
+// const [ketua, wakil, ...anggota] = kelompok1;
+// console.log(anggota);
+
+//* object destructuring
+// const team = {
+//     pm: 'Doda',
+//     frontEnd1: 'Dofa',
+//     frontEnd2: 'Dumang',
+//     backEnd: 'asep',
+//     ux: 'Dodi',
+//     devOps: 'Dadang'
+// }
+// const { pm, ...myTeam} = team;
+// console.log(myTeam);
+
+//* filtering
+function filterBy(type, ...values){
+    return values.filter(v => typeof v === type);
+}
+console.log(filterBy('boolean', 1, 2, 'Doda', false, 10, true, 'Dofa'));
