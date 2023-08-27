dd41ebc8635ed1784da47a78eda78b6c353342e3
fachry99 <151fachry@gmail.com>
2023-08-27 09:01:40 +0700

    membuat file json

---
+++ javascript ES6/callback.js
@@ -0,0 +1,43 @@
+//* callback
+// Synchronous Callback
+// function halo(nama) {
+//   alert(`halo, ${nama}`);
+// }
+
+// function tampilkanPesan(callback) {
+//     const nama = prompt("masukkan nama : ");
+//     callback(nama);
+// }
+
+// tampilkanPesan(nama => alert(`halo, ${nama}`));
+
+// Asynchronous Callback
+// const mhs = [
+//   {
+//     nama: "Sandhika Galih",
+//     nrp: "043040023",
+//     email: "sandhikagalih@unpas",
+//     jurusan: "Teknik Informatika",
+//     idDosenWali: 1,
+//   },
+//   {
+//     nama: "Doddy Ferdiansyah",
+//     nrp: "133050321",
+//     email: "doddy@unpas",
+//     jurusan: "Teknik Informatika",
+//     idDosenWali: 2,
+//   },
+//   {
+//     nama: "Erik",
+//     nrp: "133050321",
+//     email: "erik@unpas",
+//     jurusan: "Teknik Informatika",
+//     idDosenWali: 3,
+//   },
+// ];
+
+// console.log("mulai");
+// mhs.forEach(a => console.log(a.nama));
+// console.log("selesai");
+
+//* Asynchronous Callback
