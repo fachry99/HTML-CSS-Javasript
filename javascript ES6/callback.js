276578096687d4d83e7e06f7e1776127f776ec2f
fachry99 <151fachry@gmail.com>
2023-08-27 10:29:24 +0700

    buat contoh callback dengan ajax vanila dan ajax jQuerry

--- javascript ES6/callback.js
+++ javascript ES6/callback.js
@@ -40,4 +40,42 @@
 // mhs.forEach(a => console.log(a.nama));
 // console.log("selesai");
 
-//* Asynchronous Callback
+//* Asynchronous Callback (Ajax valilla javascript)
+// function getDataMahasiswa(url, success, error){
+//     let xhr = new XMLHttpRequest();
+
+//     xhr.onreadystatechange = function(){
+//         if(xhr.readyState === 4){
+//             if(xhr.status === 200){
+//                 success(xhr.response);
+//             }else if(xhr.status === 404){
+//                 error();
+//             }
+//         }
+//     }
+
+//     xhr.open('get', url);
+//     xhr.send();
+// } 
+
+
+// console.log('mulai');
+// getDataMahasiswa('data/mahasiswa.json', results => {
+//     const mhs = JSON.parse(results);
+//     mhs.forEach(m => console.log(m.nama));
+// }, () => {
+// });
+// console.log('selesai');
+
+//* Asynchronous Callback (Ajax jquery)
+console.log('mulai');
+$.ajax({
+    url: 'data/mahasiswa.json',
+    success: (mhs) => {
+        mhs.forEach(m => console.log(m.nama));
+    },
+    error: (e) => {
+        console.log(e.responseText);
+    }
+});
+console.log('selesai');
