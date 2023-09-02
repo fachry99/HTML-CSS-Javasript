53f11c994f38d1a576b1d18d10c576a325313eea
fachry99 <151fachry@gmail.com>
2023-09-03 05:58:57 +0700

    menambahkan 3 file, satu folder

---
+++ nodejs/index.js
@@ -0,0 +1,6 @@
+//console.log("Hello World again");
+//const nama = "John";
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+// console.log(cetakNama("John"));
+console.log(window);
+// const fs = require("fs");
