77c2ca4eaee3a7f66ce6bc9513c17c05c5f41141
fachry99 <151fachry@gmail.com>
2023-09-03 07:35:54 +0700

    mencoba modules

--- nodejs/coba.js
+++ nodejs/coba.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+// console.log("Hello from coba.js");
+//function
+function cetakNama(nama) {
+    return `Hi, nama saya ${nama}`;
+}
+
+//variable
+const pi = 3.14;
+
+//object
+const mahasiswa = {
+    nama: "Rizky",
+    umur: 20,
+    cetakMhs() {
+        return `Halo, nama saya ${this.nama} dan saya ${this.umur} tahun.`;
+    }
+};
+
+//class
+class Orang {
+    constructor() {
+        console.log("Objek orang telah dibuat.");
+    }
+}
+
+// module.exports.cetakNama = cetakNama;
+// module.exports.pi = pi;
+// module.exports.mahasiswa = mahasiswa;
+// module.exports.Orang = Orang;
+
+module.exports = {cetakNama, pi, mahasiswa, Orang}; // versi ES6 lebih singkat
--- nodejs/index.js
+++ nodejs/index.js
@@ -1,6 +1,11 @@
-//console.log("Hello World again");
-//const nama = "John";
-// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
-// console.log(cetakNama("John"));
-console.log(window);
-// const fs = require("fs");
+// const fs = require("fs"); // core module
+// const cetakNama = require("./coba"); // local module
+// const moment = require("moment"); // third party module / npm module / node_modules
+
+const coba = require("./coba");
+console.log(
+    coba.cetakNama("fahri"), 
+    coba.pi, 
+    coba.mahasiswa.cetakMhs(), 
+    new coba.Orang()
+);
