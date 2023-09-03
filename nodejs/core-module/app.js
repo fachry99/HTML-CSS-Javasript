947e3d9ec719b5df9d38a9228513a8e2df66c72e
fachry99 <151fachry@gmail.com>
2023-09-03 09:45:52 +0700

    update

--- nodejs/core-module/app.js
+++ nodejs/core-module/app.js
@@ -23,3 +23,35 @@ const fs = require("fs");
 //     if(err) throw err;
 //     console.log(data);
 // });
+
+//*readLine 
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Masukkan nama anda: ", (nama) => {
+    rl.question("Masukkan no HP anda: ", (noHP) => {
+        const contact = { nama, noHP };
+        const file = fs.readFileSync("contacts.json", "utf-8");
+        const contacts = JSON.parse(file); //mengubah string menjadi objek javascript
+
+        contacts.push(contact);
+
+        fs.writeFileSync("contacts.json", JSON.stringify(contacts));
+        console.log("Terima Kasih sudah memasukkan data.");
+        rl.close();
+    });
+}
+);
+
+// Path: nodejs/core-module/contacts.json
+// [
+//     {
+//         "nama": "Rizky",
+//         "noHP": "08123456789"
+//     },
+//     {
+//         "nama": "Rizky",
+//         "noHP": "08123456789"
